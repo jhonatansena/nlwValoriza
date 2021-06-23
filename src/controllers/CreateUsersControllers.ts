aa2cee31b177a94e9b332d8b7710f2ab0c04bef4
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-23 16:37:43 -0300

    finalizado a parte de controllers de criacao de usuario

--- src/controllers/CreateUsersControllers.ts
+++ src/controllers/CreateUsersControllers.ts
@@ -1,8 +1,17 @@
 import {Response, Request} from "express"
+import { CreateUserService } from "../services/CreateUserService";
 
 class CreateUsersControllers{
 
     async handle(request: Request, response: Response){
+        const {name, email, admin} = request.body;
+
+        const createUserService = new CreateUserService();
+
+        const user = await createUserService.execute({name, email, admin});
+
+        return response.json(user);
+
 
     }
 
