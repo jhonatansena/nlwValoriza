9596673afb455c2175ad5edf56353bf02ed788c6
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-23 16:38:38 -0300

    chamando o router no server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import "reflect-metadata"
 import express from "express";
+import { router } from "./router";
 
 import "./database"
 
 
 const app = express();
 const port = 3000;
+
+app.use(router);
 
 app.listen(port, () => {
     console.log("Server is running on port ", port);
