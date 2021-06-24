c8646807aba5fce879d477134603575a10006088
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-24 20:29:54 -0300

    Rota para autenticar usuario

--- src/router.ts
+++ src/router.ts
@@ -3,16 +3,19 @@ import { Router } from "express";
 import { CreateUsersControllers } from "./controllers/CreateUsersControllers";
 import { CreateTagsControllers } from "./controllers/CreateTagsControllers"
 import {ensureAdmin} from "./middlewares/ensureAdmin"
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 
 const router = Router();
 
 const createUsersControllers = new CreateUsersControllers();
 const createTagsControllers = new CreateTagsControllers();
+const authenticateUserController = new AuthenticateUserController();
+
 
 
 router.post("/tags", ensureAdmin, createTagsControllers.handle);
 router.post("/users", createUsersControllers.handle);
-
+router.post("/login", authenticateUserController.handle)
 
 export {router};
