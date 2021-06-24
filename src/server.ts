d1b44cafd0ad49527614140911ab95ac15930bf0
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-24 13:50:48 -0300

    biblioteca para tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata"
-import express from "express";
+import express, {Request, Response, NextFunction } from "express";
+import "express-async-errors";
 
 import { router } from "./router";
 
@@ -12,6 +13,19 @@ app.use(express.json());
 
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error){
+        return response.status(400).json({
+            err: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
 
 const port = 3000;
 app.listen(port, () => {
