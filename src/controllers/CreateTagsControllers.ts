29cc8c48cf75abf3bbea10f45478b389a187eb8b
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-24 14:32:56 -0300

    criaçao de controller de tas

---
+++ src/controllers/CreateTagsControllers.ts
@@ -0,0 +1,17 @@
+import {Request, Response} from "express"
+import {CreateTagsService} from "../services/CreateTagsService"
+
+class CreateUsersControllers{
+
+    async handle(request: Request, response: Response){
+        const name = request.body;
+
+        const createTagsService = new CreateTagsService();
+
+        const tag = await createTagsService.execute(name);
+
+        return response.json(tag)
+    }
+}
+
+export {CreateUsersControllers}
