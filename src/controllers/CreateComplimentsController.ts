58dbc997dfb6ba231816b384eee007e671c04d81
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-25 17:48:21 -0300

    Lista de compliments por usuarios

--- src/controllers/CreateComplimentsController.ts
+++ src/controllers/CreateComplimentsController.ts
@@ -4,11 +4,17 @@ import { CreateComplimentsService } from "../services/CreateComplimentsService";
 class CreateComplimentsController{
 
     async handle(request: Request, response: Response){
-        const {tag_id, user_sender, user_receiver, message} = request.body;
-        
+        const {tag_id, user_receiver, message} = request.body;
+        const {user_id} = request 
+
         const createComplimentsService = new CreateComplimentsService();
         
-        const compliment = await createComplimentsService.execute({tag_id, user_sender, user_receiver, message});
+        const compliment = await createComplimentsService.execute({
+            tag_id,
+            user_sender: user_id,
+            user_receiver,
+            message
+        });
 
         return response.json(compliment);
     }
